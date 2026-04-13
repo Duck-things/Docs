f2281fa952e82be09b9ae6ec2e7ef77345cdb0fb
awesumdude42 <sammyiyers@gmail.com>
2026-04-12 17:16:45 -0700

    did stuff

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://apexpathing.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -71,7 +71,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/Social-Card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
